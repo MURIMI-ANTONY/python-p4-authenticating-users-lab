7cd3883a59279c948940398f88dfb2c1efa456a8
MURIMI-ANTONY <amurimi101@gmail.com>
2024-02-19 19:59:37 +0300

    done

--- server/app.py
+++ server/app.py
@@ -18,6 +18,24 @@ db.init_app(app)
 
 api = Api(app)
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id']= user.id
+        return user.to_dict(),200
+    
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session['user_id']).first()
+        if user:
+            return make_response(jsonify(user.to_dict()),200)
+        return {}, 401
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -51,6 +69,9 @@ class ShowArticle(Resource):
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession,'/check_session')
 
 
 if __name__ == '__main__':
